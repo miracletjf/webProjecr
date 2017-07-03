c85fd8b04dc806e5701f220b1bcafdf2456e3a4f
miracletjf <15715716061@163.com>
2017-07-03 17:23:22 +0800

    add stotageCapacity

---
+++ storageCapacity/js/vue_getDate.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+var containerBox = new Vue({
+    el : "#container_box",
+    data: {
+        storages: {},
+        tuli: {}
+    },
+    methods: {
+        dataSearch: function(objid){
+            var _self = this;
+            $.ajax({
+                type: 'get',
+                url: 'http://127.0.0.1:8180/wmsGUI.pr.spGUIExpand.do',
+                dataType: 'jsonp',
+                jsonp: 'callback',
+                data: {
+                    "param/sp_id": objid
+                },
+                success: function (data) {
+                    _self.storages = data.sh;
+                    _self.tuli = data.gb;
+                    console.log(data);
+                }
+            });
+        }
+    },
+    filters : {
+        statusType: function (value) {
+            if(value == 'P'){
+                return '';
+            }
+            return 'status';
+        },
+        pz: function (value) {
+            if (value == 83 || value == 85) {
+                return 'xiaomai';
+            } else if (value == 86) {
+                return 'yumi';
+            } else {
+                return 'dadou';
+            }
+        }
+
+    }
+});
+
+containerBox.dataSearch(warehouseId);
